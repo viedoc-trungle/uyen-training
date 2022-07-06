9839b54001645ee47aba2519824cbea326579ee2
uyenpham-viedoc <108775671+uyenpham-viedoc@users.noreply.github.com>
2022-07-06 12:59:54 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num1;// Biến chứa số nhập vào từ bàn phím
+            int num2;
+
+            Console.Write("So thu 1: ");
+            num1 = int.Parse(Console.ReadLine()); 
+
+            Console.Write("So thu 2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Tong 2 so la: ");
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadKey();
+        }
+    }
+}
