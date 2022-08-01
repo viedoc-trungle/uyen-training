23c75fa36071d7e562c178251002451d314efd94
Pham Thi Phuong Uyen <uyen.pham@viedoc.com>
2022-08-01 08:51:07 +0700

    Uyen Pham's lesson 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,25 +1,66 @@
 ﻿using System;
+using Microsoft.VisualBasic;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            
-            int num1;// Biến chứa số nhập vào từ bàn phím
-            int num2;
-
             Console.Write("So thu 1: ");
-            num1 = int.Parse(Console.ReadLine()); 
+            int num1 = int.Parse(Console.ReadLine()); 
 
             Console.Write("So thu 2: ");
-            num2 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
 
-            Console.Write("Tong 2 so la: ");
-            Console.WriteLine(num1 + num2);
+            Console.Write("Thuc hien phep tinh: ");
+            string toantu = Console.ReadLine();
 
-            Console.ReadKey();
+            if (toantu == "+")
+            {
+                Phepcong(num1, num2);
+            }
+            else if (toantu == "-")
+            {
+                Pheptru(num1, num2);
+            }
+            else if (toantu == "*")
+            {
+                Phepnhan(num1, num2);
+            }
+
+            else if (toantu == "/")
+            {
+                Phepchia(num1, num2);
+            }
+
         }
+       static void Phepcong(int num1, int num2)
+       {
+           Console.WriteLine("Tong cua 2 so la:" + (num1 + num2));
+           
+        }
+       static void Pheptru(int num1, int num2)
+       { 
+           Console.WriteLine("Hieu của 2 so la:" + (num1 - num2));
+          
+       }
+        static void Phepnhan(int num1, int num2)
+       {
+         Console.WriteLine("Tich của 2 so la:" + (num1 * num2));
+            
+        }
+        static void Phepchia(int num1, int num2)
+       {
+            if ( num2 != 0)
+            {
+                Console.WriteLine("Thuong cua 2 so la:" + (num1 / num2));
+            }
+            else if (num2 == 0)
+
+            {
+                Console.WriteLine("Khong the chia cho 0");
+            }
+       }
     }
 }
